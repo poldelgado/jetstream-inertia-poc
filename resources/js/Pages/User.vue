15f663c8dd84ddd0c443425cf8b982c968a6edf9
Pablo Delgado <poldelgado@gmail.com>
2022-03-24 14:23:17 -0300

    first commit

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <app-layout>
+        <div class="card">
+            <div class="card-header bg-info">
+                Hola Mundo!
+            </div>
+            <div class="card-body">
+                <pre>
+                    {{users}}
+                </pre>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+import AppLayout from "@/Layouts/AppLayout.vue"
+
+export default {
+    props: ['users'],
+    components: {
+        AppLayout,
+    },
+    setup() {
+
+    },
+}
+</script>
